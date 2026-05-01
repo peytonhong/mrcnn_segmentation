721120200692a190ea212d65633d923fd088f4b9
peytonhong <hhs8314@gmail.com>
2026-05-02 02:17:02 +0900

    'optim'

--- mrcnn_segmentation.py
+++ mrcnn_segmentation.py
@@ -69,7 +69,7 @@ def main(args):
     params = [p for p in model.parameters() if p.requires_grad]
     # optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
     optimizer = torch.optim.Adam(params, lr=0.001)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
 
     num_epochs = args.num_epochs
 
